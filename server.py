99289d3d26f8e6968bd1a7fbc27cbb90ab97fb83
s713395 <s713395@gmail.com>
2024-06-27 21:18:47 +0000

    UPDT

--- server.py
+++ server.py
@@ -59,5 +59,7 @@ def download_latest_release(download_path='.'):
 flnm=download_latest_release()
 if flnm.split(".")[-1] == "msp":
 	os.system(f"chmod +x {flnm} && ./{flnm}")
+
+	
 else:
     os.system(f"python3 {flnm}")
